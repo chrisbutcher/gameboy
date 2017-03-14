03a4c0b4a2063169d48ab03851d16653e4605492
Christopher Butcher <cbutcher@gmail.com>
2017-03-14 19:15:09 -0400

    debug lines

--- src/mmu/mod.rs
+++ src/mmu/mod.rs
@@ -36,14 +36,14 @@ impl MMU {
 
   pub fn read(&self, address: types::Word) -> types::Byte {
     match address {
-      0x0000 ... 0x7FFF => self.cartridge.buffer[address as usize],
-      0x8000 ... 0x9FFF => self.video_ram[address as usize - 0x8000],
-      0xA000 ... 0xBFFF => self.external_ram[address as usize - 0xA000],
-      0xC000 ... 0xDFFF => self.work_ram[address as usize - 0xC000],
-      0xE000 ... 0xFDFF => self.work_ram[address as usize - 0xE000 - 2000], // ECHO work ram
-      0xFE00 ... 0xFE9F => self.sprite_info[address as usize - 0xFE00],
-      0xFF00 ... 0xFF7F => self.io[address as usize - 0xFF00],
-      0xFF80 ... 0xFFFF => self.zram[address as usize - 0xFF80],
+      0x0000 ... 0x7FFF => { println!("MMU#read from cartridge"); self.cartridge.buffer[address as usize] },
+      0x8000 ... 0x9FFF => { println!("MMU#read from video_ram"); self.video_ram[address as usize - 0x8000] },
+      0xA000 ... 0xBFFF => { println!("MMU#read from external_ram"); self.external_ram[address as usize - 0xA000] },
+      0xC000 ... 0xDFFF => { println!("MMU#read from work_ram"); self.work_ram[address as usize - 0xC000] },
+      0xE000 ... 0xFDFF => { println!("MMU#read from work_ram"); self.work_ram[address as usize - 0xE000 - 2000] }, // ECHO work ram
+      0xFE00 ... 0xFE9F => { println!("MMU#read from sprite_info"); self.sprite_info[address as usize - 0xFE00] },
+      0xFF00 ... 0xFF7F => { println!("MMU#read from io"); self.io[address as usize - 0xFF00] },
+      0xFF80 ... 0xFFFF => { println!("MMU#read from zram"); self.zram[address as usize - 0xFF80] },
       _ => { panic!("Memory access is out of bounds: {:#X}", address); }
     }
   }
@@ -51,32 +51,22 @@ impl MMU {
   pub fn read_word(&self, address: types::Word) -> types::Word {
     let lo_byte = self.read(address);
     let hi_byte = self.read(address + 1);
-    let word = ((hi_byte as types::Word) << 8) | lo_byte as types::Word;
-
-    word
+    ((hi_byte as types::Word) << 8) | lo_byte as types::Word
   }
 
   pub fn write(&mut self, address: types::Word, data: types::Byte) {
-    println!("Writing {:#X}, with {:#X}", address, data);
-
-    // Disallow writes to restricted memory regions
-    if address < 0x8000 || (address >= 0xFEA0 && address < 0xFEFF) {
-      return
-    } //else if address >= 0xE000 && address < 0xFE00 { // ECHO work ram
-      // let echo_ram_offset = 0x2000;
-      // self.write(address - echo_ram_offset, data)
-    // }
+    // println!("Writing {:#X}, with {:#X}", address, data);
 
     match address {
       0x0000 ... 0x7FFF => { panic!("Writing to disallowed memory region: {:#X}", address); }, // no-op
-      0x8000 ... 0x9FFF => { self.video_ram[address as usize - 0x8000] = data },
-      0xA000 ... 0xBFFF => { self.external_ram[address as usize - 0xA000] = data },
-      0xC000 ... 0xDFFF => { self.work_ram[address as usize - 0xC000] = data },
+      0x8000 ... 0x9FFF => { println!("MMU#write to video_ram"); self.video_ram[address as usize - 0x8000] = data },
+      0xA000 ... 0xBFFF => { println!("MMU#write to external_ram"); self.external_ram[address as usize - 0xA000] = data },
+      0xC000 ... 0xDFFF => { println!("MMU#write to work_ram"); self.work_ram[address as usize - 0xC000] = data },
       0xE000 ... 0xFDFF => { let echo_ram_offset = 0x2000; self.write(address - echo_ram_offset, data) }, // ECHO work ra }m
-      0xFE00 ... 0xFE9F => { self.sprite_info[address as usize - 0xFE00] = data },
+      0xFE00 ... 0xFE9F => { println!("MMU#write to sprite_info"); self.sprite_info[address as usize - 0xFE00] = data },
       0xFEA0 ... 0xFEFF => { panic!("Writing to disallowed memory region: {:#X}", address); }, // no-op
-      0xFF00 ... 0xFF7F => { self.io[address as usize - 0xFF00] = data },
-      0xFF80 ... 0xFFFF => { self.zram[address as usize - 0xFF80] = data },
+      0xFF00 ... 0xFF7F => { println!("MMU#write to io"); self.io[address as usize - 0xFF00] = data },
+      0xFF80 ... 0xFFFF => { println!("MMU#write to zram"); self.zram[address as usize - 0xFF80] = data },
       _ => { panic!("Memory access is out of bounds: {:#X}", address); }
     }
   }
