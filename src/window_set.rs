26d407822ef0ff3269b821a0a3d648235b5c28dd
Christopher Butcher <cbutcher@gmail.com>
2017-12-28 18:31:20 -0500

    optimized video rendering

--- src/window_set.rs
+++ src/window_set.rs
@@ -1,8 +1,10 @@
 pub use super::sdl2;
 pub use super::sdl2::pixels;
 pub use super::sdl2::pixels::PixelFormatEnum;
+pub use super::sdl2::rect::Rect;
 
 pub use super::sdl2::render::Texture;
+pub use super::sdl2::render::TextureAccess;
 pub use super::sdl2::render::TextureCreator;
 pub use super::sdl2::render::WindowCanvas;
 
@@ -49,29 +51,30 @@ impl WindowSet {
     (window, sdl_context)
   }
 
-  // Perf: Profiled with profile.release setting in Cargo.toml, and used XCode tool Instruments. Found that draw_point and Point::new dominated
+  // Perf, in previous iteration: Profiled with profile.release setting in Cargo.toml, and used XCode tool Instruments.
+  // Found that draw_point and Point::new dominated
+
   pub fn render_screen(&mut self, framebuffer: &[u8]) {
     if !RENDER_PIXELS { return }
 
-    for y in 0..144 {
-      for x in 0..160 {
-        let framebuffer_index = (y * 4) * 160 + (x * 4);
+    let game_texture_creator = self.game_canvas.texture_creator();
+    let mut game_texture = game_texture_creator.create_texture_streaming(PixelFormatEnum::ABGR8888, 160, 144).unwrap();
 
-        let pixel_r = framebuffer[ framebuffer_index as usize ];
-        let pixel_g = framebuffer[ framebuffer_index as usize + 1 ];
-        let pixel_b = framebuffer[ framebuffer_index as usize + 2 ];
-        let pixel_a = framebuffer[ framebuffer_index as usize + 3 ];
+    game_texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
+      for y in 0..144 {
+        for x in 0..160 {
+          let offset = y * pitch + x * 4;
 
-        // match self.game_canvas {
-          // Some(ref mut canvas) => {
-            self.game_canvas.set_draw_color(pixels::Color::RGBA(pixel_r, pixel_g, pixel_b, pixel_a));
-            self.game_canvas.draw_point(sdl2::rect::Point::new(x, y)).unwrap()
-          // }
-          // _ => {}
-        // }
+          buffer[offset] = framebuffer[offset];
+          buffer[offset + 1] = framebuffer[offset + 1];
+          buffer[offset + 2] = framebuffer[offset + 2];
+          buffer[offset + 3] = framebuffer[offset + 3];
+        }
       }
-    }
+    }).unwrap();
 
+    self.game_canvas.clear();
+    self.game_canvas.copy(&game_texture, None, Some(Rect::new(0, 0, 160, 144))).unwrap();
     self.game_canvas.present();
   }
 
