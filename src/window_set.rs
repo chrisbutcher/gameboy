08d3c6d9424cd9b8cc1779245782e4f58d286bbd
Christopher Butcher <cbutcher@gmail.com>
2018-01-28 15:15:53 -0500

    scale windows

--- src/window_set.rs
+++ src/window_set.rs
@@ -8,6 +8,14 @@ pub use super::sdl2::video::{WindowContext, WindowPos};
 
 const RENDER_PIXELS: bool = true;
 
+const GAME_WINDOW_WIDTH_BASE: u32 = 160;
+const GAME_WINDOW_HEIGHT_BASE: u32 = 144;
+const GAME_WINDOW_SCALE: f32 = 3.0;
+
+const DEBUG_WINDOW_WIDTH_BASE: u32 = 192;
+const DEBUG_WINDOW_HEIGHT_BASE: u32 = 128;
+const DEBUG_WINDOW_SCALE: f32 = 3.0;
+
 pub struct WindowSet {
   pub sdl_context: sdl2::Sdl,
 
@@ -19,15 +27,26 @@ impl WindowSet {
   pub fn new() -> WindowSet {
     let sdl_context = sdl2::init().unwrap();
 
-    let (debug_window, sdl_context) = WindowSet::new_window(sdl_context, "DEBUG", 192, 128, 180);
-    let (game_window, sdl_context) = WindowSet::new_window(sdl_context, "GAMEBOY", 160, 144, 0);
+    let (debug_window, sdl_context) = WindowSet::new_window(
+      sdl_context,
+      "DEBUG",
+      DEBUG_WINDOW_WIDTH_BASE * DEBUG_WINDOW_SCALE as u32,
+      DEBUG_WINDOW_HEIGHT_BASE * GAME_WINDOW_SCALE as u32,
+      180
+    );
 
-    let game_canvas = game_window.into_canvas().accelerated().present_vsync().build().unwrap();
+    let (game_window, sdl_context) = WindowSet::new_window(sdl_context, "GAMEBOY", GAME_WINDOW_WIDTH_BASE * DEBUG_WINDOW_SCALE as u32, GAME_WINDOW_HEIGHT_BASE * DEBUG_WINDOW_SCALE as u32, 0);
+
+    let mut game_canvas = game_window.into_canvas().accelerated().present_vsync().build().unwrap();
+    game_canvas.set_scale(GAME_WINDOW_SCALE, GAME_WINDOW_SCALE);
+
+    let mut debug_canvas = debug_window.into_canvas().accelerated().present_vsync().build().unwrap();
+    debug_canvas.set_scale(GAME_WINDOW_SCALE, GAME_WINDOW_SCALE);
 
     WindowSet {
       sdl_context: sdl_context,
       game_canvas: game_canvas,
-      debug_canvas: debug_window.into_canvas().accelerated().present_vsync().build().unwrap()
+      debug_canvas: debug_canvas
     }
   }
 
